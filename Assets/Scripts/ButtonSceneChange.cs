2a8fcfae1c9d3d5e97b95af867e689abb9d989a9
Evan100354 <evanlong100354@gmail.com>
2023-10-25 13:16:21 +0100

    I am become UI guy, bringer of menus

---
+++ Assets/Scripts/ButtonSceneChange.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSceneChange : MonoBehaviour
+{
+
+    public string Scene;
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+}
