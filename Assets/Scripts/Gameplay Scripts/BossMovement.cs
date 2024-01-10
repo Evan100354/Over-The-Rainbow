fa46203a846bad981170d056cd3e051adffbf819
LukeN25 <C22532639@mytudublin.ie>
2024-01-10 17:23:29 +0000

    many things done
    He runs now

--- Assets/Scripts/Gameplay Scripts/BossMovement.cs
+++ Assets/Scripts/Gameplay Scripts/BossMovement.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class BossMovement : MonoBehaviour
 {
-    public float bossspeed = 5f;
+    public float bossspeed = 3f;
     public Rigidbody rbBoss;
+    public bool running = false;
 
+    private void Start()
+    {
+        StartCoroutine(RunDelay(5));
+    }
     private void FixedUpdate()
     {
-        Vector3 forwardMove = transform.forward * bossspeed * Time.fixedDeltaTime;
+        if (running)
+        {
+            Vector3 forwardMove = transform.forward * bossspeed * Time.fixedDeltaTime;
 
-        rbBoss.MovePosition(rbBoss.position + forwardMove);
+            rbBoss.MovePosition(rbBoss.position + forwardMove);
+        }
+    }
+
+    IEnumerator RunDelay(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+        running = true;
     }
 }
