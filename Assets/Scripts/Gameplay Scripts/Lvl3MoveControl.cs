04afee194f5a849c6b2e5ff1c40dc5f64ae6dceb
Evan100354 <evanlong100354@gmail.com>
2024-01-12 18:26:22 +0000

    God help us

---
+++ Assets/Scripts/Gameplay Scripts/Lvl3MoveControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl3MoveControl : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + forwardMove);
+    }
+}
